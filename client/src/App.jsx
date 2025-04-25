e4144d3e46326ba86a1c3801754dc18340dbb7b0
Soumadip <39thdecember@gmail.com>
2025-04-25 22:32:18 +0530

    NavBar using prebuiltUI ☑️

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
