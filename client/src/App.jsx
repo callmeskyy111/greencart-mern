848ce9db7b316d30a2ba6592c121a0df17fa8cb7
Soumadip <39thdecember@gmail.com>
2025-04-26 18:23:33 +0530

    Login-SignUp Form ✅

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,17 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import { useAppContext } from "./context/AppContext";
+import Login from "./components/Login";
 
 function App() {
+  const { showUserLogin } = useAppContext();
   //checking if it's 'seller' path or not
   const isSellerPtah = useLocation().pathname.includes("seller");
   return (
     <div>
       {isSellerPtah ? null : <Navbar />}
+      {showUserLogin ? <Login /> : null}
       <Toaster />
       <div
         className={`${isSellerPtah ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
@@ -25,4 +29,4 @@ function App() {
 
 export default App;
 
-//02:03
+//02:25:15
