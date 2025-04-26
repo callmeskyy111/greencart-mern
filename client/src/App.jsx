f45ddc5cd398d24d6b63867dca8e22d6a0f1d187
Soumadip <39thdecember@gmail.com>
2025-04-26 18:01:36 +0530

    Home Page UI Done ☑️

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import { Route, Routes, useLocation } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import Home from "./pages/Home";
+import Footer from "./components/Footer";
 
 function App() {
   //checking if it's 'seller' path or not
@@ -15,10 +16,9 @@ function App() {
         className={`${isSellerPtah ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route />
-          <Route />
         </Routes>
       </div>
+      {!isSellerPtah && <Footer />}
     </div>
   );
 }
