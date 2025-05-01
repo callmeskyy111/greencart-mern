b6d81f26bab64cb33e2832381c6bd44ea78466d2
Soumadip <39thdecember@gmail.com>
2025-05-01 14:02:26 +0530

    registerUser and loginUser controllers ☑️

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -35,3 +35,49 @@ export async function registerUser(req, res) {
     res.json({ success: false, message: err.message });
   }
 }
+
+// Login User
+
+export async function loginUser(req, res) {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      res.json({
+        success: false,
+        message: "Both EMAIL & PASSWORD are required! 🔴",
+      });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.json({
+        success: false,
+        message: "Invalid EMAIL or PASSWORD! 🔴",
+      });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.json({
+        success: false,
+        message: "Invalid email or password! 🔴",
+      });
+    }
+    //token generation
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "3d",
+    });
+    res.cookie("token", token, {
+      httpOnly: true, // Prevent JS to access the cookie
+      secure: process.env.NODE_ENV === "production", // Use secure cookies in production
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict", // CSRF protection
+      maxAge: 7 * 24 * 60 * 60 * 1000, // Cookie expiration time in ms.
+    });
+    return res.json({
+      success: true,
+      message: "User Successfully Logged In ✅",
+      user: { email: user.email, name: user.name },
+    });
+  } catch (err) {
+    console.log("🔴 COMPLETE ERROR: ", err);
+    res.json({ success: false, message: err.message });
+  }
+}
